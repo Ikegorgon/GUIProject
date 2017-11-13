00657ccd5c8eab808a83e02957d0f1df076ed851
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-11-13 12:31:20 -0700

    Made GUIFrame (Base Frame)
    always need super(); & this.setVisible(true);

---
+++ src/GUI/view/GUIFrame.java
@@ -0,0 +1,23 @@
+package GUI.view;
+
+import GUI.controller.*;
+import javax.swing.JFrame;
+
+public class GUIFrame extends JFrame {
+	private GUIController appController;
+	private GUIPanel appPanel;
+	
+	public GUIFrame(GUIController appController) {
+		super();
+		this.appController = appController;
+		this.appPanel = new GUIPanel(appController);
+		setupFrame();
+		
+	}
+	private void setupFrame() {
+		this.setContentPane(appPanel);
+		this.setTitle("Title goes here");
+		this.setSize(500, 500);
+		this.setVisible(true);
+	}
+}
