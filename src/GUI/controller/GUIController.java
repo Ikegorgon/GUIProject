46e7f22c726925f77720e1a2078e3e555caef5f5
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-11-13 13:16:19 -0700

    Made GUI Controller

--- src/GUI/controller/GUIController.java
+++ src/GUI/controller/GUIController.java
@@ -1,6 +1,14 @@
 package GUI.controller;
 
+import GUI.view.*;
+
 public class GUIController {
+	private GUIFrame appFrame;
+	
+	public GUIController() {
+			appFrame = new GUIFrame(this);
+		}
+	
 	public void start() {
 		
 	}
