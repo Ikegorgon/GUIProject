426b5b255d79be90bf7feaf91f3a7f644ff114ab
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-11-13 12:58:58 -0700

    Created GUIPanel to design app

---
+++ src/GUI/view/GUIPanel.java
@@ -0,0 +1,41 @@
+package GUI.view;
+
+import javax.swing.*;
+import GUI.controller.*;
+import java.awt.Color;
+
+public class GUIPanel extends JPanel {
+	private GUIController appController;
+	private JButton firstButton;
+	private SpringLayout appLayout;
+	public GUIPanel(GUIController appController) {
+		super();
+		this.appController = appController;
+		firstButton = new JButton("Click on the button");
+		appLayout = new SpringLayout();
+		
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	/**
+	 * Used to add all components to the screen and install a layout manager.
+	 * Also set color :D
+	 */
+	private void setupPanel() {
+		this.setBackground(Color.CYAN);
+		this.setLayout(appLayout);
+		this.add(firstButton);
+	}
+	private void setupLayout() {
+		
+		appLayout.putConstraint(SpringLayout.NORTH, firstButton, 200, SpringLayout.NORTH, this);
+		appLayout.putConstraint(SpringLayout.WEST, firstButton, 150, SpringLayout.WEST, this);
+		appLayout.putConstraint(SpringLayout.SOUTH, firstButton, 250, SpringLayout.NORTH, this);
+		appLayout.putConstraint(SpringLayout.EAST, firstButton, 350, SpringLayout.WEST, this);
+	}
+	private void setupListeners() {
+		
+	}
+}
